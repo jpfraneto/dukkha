987d4e5f8113d2e329bad543e50a194fe7432bc4
jpfraneto <jpfraneto@gmail.com>
2022-09-22 10:40:44 -0300

    added title

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,15 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>DUKKHA</h1>
-      <h2>human evolution</h2>
-      <p>This universe is coming into being...</p>
-    </div>
+    <>
+      <Head>
+        <title>Dukkha · Human Evolution</title>
+      </Head>
+      <div className={styles.container}>
+        <h1>DUKKHA</h1>
+        <h2>human evolution</h2>
+        <p>This universe is coming into being...</p>
+      </div>
+    </>
   );
 }
