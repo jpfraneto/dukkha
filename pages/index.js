31b9a8bb2d7c8f34e7167bdd3ff9bce6c20b30b1
jpfraneto <jpfraneto@gmail.com>
2022-09-22 14:49:41 -0300

    added message on the bottom

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,12 @@ export default function Home() {
         <h1>DUKKHA</h1>
         <h2>human evolution</h2>
         <p>This universe is coming into being...</p>
+        <small>
+          Im looking for funding for this project. I just need 10k usd to
+          survive for the next 6 months and I will create something amazing that
+          will help a lot of people. If you are interested in supporting this
+          journey, please reach me out at jpfraneto@gmail.com
+        </small>
       </div>
     </>
   );
