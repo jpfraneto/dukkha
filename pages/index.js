b36ba3e0f2497d0c7cd2953f9eef32920e9175aa
jpfraneto <jpfraneto@gmail.com>
2022-09-25 23:22:51 -0300

    fixed text

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <h2>human evolution</h2>
         <p>This universe is coming into being...</p>
         <small>
-          I'm looking for funding for this project. I just need 10k usd to
+          I&apos;m looking for funding for this project. I just need 10k usd to
           survive for the next 6 months and I will create something amazing that
           will help a lot of people. If you are interested in supporting this
           journey, please reach me out at jpfraneto@gmail.com with the subject
