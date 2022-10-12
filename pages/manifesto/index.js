ec0aaf88519c597dd3058f2cc2d1b0daae392c08
jpfraneto <jpfraneto@gmail.com>
2022-10-12 13:20:44 -0300

    added day 17

--- pages/manifesto/index.js
+++ pages/manifesto/index.js
@@ -4,7 +4,7 @@ import Head from 'next/head';
 
 const ManifestoIndex = () => {
   const manifestosDays = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
   ];
   return (
     <>
