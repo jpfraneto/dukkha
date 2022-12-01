25953621e6674d2c3d38c1b6672554ac911ce1ca
jpfraneto <jpfraneto@gmail.com>
2022-12-01 15:07:06 -0300

    added days 66 and 67

--- pages/manifesto/index.js
+++ pages/manifesto/index.js
@@ -5,11 +5,15 @@ import styles from '../../styles/ManifestoIndex.module.css';
 import { allManifestoEntries } from 'contentlayer/generated';
 
 const ManifestoIndex = () => {
+  const words = allManifestoEntries.reduce((acc, val) => {
+    return acc + val.words;
+  }, 0);
+  console.log(words);
   const manifestosDays = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
     41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-    60, 61, 62, 63, 64, 65,
+    60, 61, 62, 63, 64, 65, 66, 67,
   ];
 
   return (
@@ -22,6 +26,12 @@ const ManifestoIndex = () => {
           I&apos;m writing this book every day until I get to day number 88.
           What you will find here is just the raw draft, without any kind of
           editing.
+        </p>
+        <p>
+          There have been {words} written in this journey of deep exploration
+          into myself. I'm not doing this for you to read it, I know it is too
+          much, I'm just doing to process the unfolding that is happening within
+          me.
         </p>
         <br />
         <div className={styles.daysList}>
