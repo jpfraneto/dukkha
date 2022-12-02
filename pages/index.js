2b170eb4974ea25d5b3691299bb5d9e88abf91cb
jpfraneto <jpfraneto@gmail.com>
2022-12-02 15:04:54 -0300

    added day 68

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,82 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import { allManifestoEntries } from 'contentlayer/generated';
 
 export default function Home() {
+  const [displayedRandomParagraph, setDisplayedRandomParagraph] =
+    useState(null);
+  const [displayedRandomEntryNumber, setDisplayedRandomEntryNumber] =
+    useState(null);
+  const getRandomParagraph = () => {
+    const totalEntries = allManifestoEntries.length;
+    const randomNumber = Math.floor(totalEntries * Math.random());
+    const randomEntry = allManifestoEntries[randomNumber];
+    const randomEntryBody = randomEntry.body.raw.split('\n');
+    setDisplayedRandomEntryNumber(randomEntry.index);
+    const newRandomNumber = Math.floor(randomEntryBody.length * Math.random());
+    const randomParagraph = randomEntryBody[newRandomNumber];
+    while (
+      randomParagraph === '' ||
+      randomParagraph === ' ' ||
+      randomParagraph === '--'
+    ) {
+      newRandomNumber = Math.floor(randomEntryBody.length * Math.random());
+      randomParagraph = randomEntryBody[newRandomNumber];
+    }
+    setDisplayedRandomParagraph(randomParagraph);
+  };
   return (
     <>
       <Head>
         <title>Dukkha</title>
       </Head>
-      <div className={styles.container}>
-        <div className={styles.topContainer}>
-          <h1>DUKKHA</h1>
-          <h2>· the only way in is through ·</h2>
+      <>
+        <div className={styles.container}>
+          <div className={styles.topContainer}>
+            <h1>DUKKHA</h1>
+            <h2>· the only way in is through ·</h2>
+          </div>
+          <div className={styles.middleText}>
+            <p>
+              I write so that I can better understand what is going on inside
+              me.
+            </p>
+            <p>I write to stop trying to make sense.</p>
+            <p>I write so that I can embody who I am.</p>
+            <p>I write so that I can help others.</p>
+            <p>I write to be more of myself.</p>
+            <p>I write to share my truth.</p>
+          </div>{' '}
+          <div className={styles.bookLinks}>
+            {' '}
+            <Link href='/manifesto'>
+              <a className={styles.getRandomParagraphBtn}>Read</a>
+            </Link>{' '}
+            <a
+              className={styles.getRandomParagraphBtn}
+              onClick={getRandomParagraph}
+            >
+              Random
+            </a>
+          </div>
         </div>
-        <div className={styles.middleText}>
-          <p>
-            I write so that I can better understand what is going on inside me.
-          </p>
-          <p>I write to stop trying to make sense.</p>
-          <p>I write so that I can embody who I am.</p>
-          <p>I write so that I can help others.</p>
-          <p>I write to be more of myself.</p>
-          <p>I write to share my truth.</p>
-        </div>{' '}
-        <div className={styles.bookLinks}>
-          {' '}
-          <Link href='/manifesto'>
-            <a>Read</a>
-          </Link>{' '}
-          ({' '}
-          <a
-            href={`https://jpfraneto.gumroad.com/l/dukkha`}
-            target='_blank'
-            rel='noreferrer'
-            className={styles.paywallBtn}
-          >
-            Get the book
-          </a>{' '}
-          |{' '}
-          <a
-            href={`https://github.com/jpfraneto/dukkha/tree/main/data/manifesto`}
-            target='_blank'
-            rel='noreferrer'
-            className={styles.paywallBtn}
-          >
-            Edit on Github
-          </a>
-          )
-        </div>
-      </div>
+        {displayedRandomParagraph && (
+          <div className={styles.randomParagraphContainer}>
+            <h2>
+              This is from{' '}
+              <Link href={`/manifesto/day-${displayedRandomEntryNumber}`}>
+                <a className={styles.dayLinkBtn}>
+                  day {displayedRandomEntryNumber}
+                </a>
+              </Link>
+            </h2>
+            <p>{displayedRandomParagraph}</p>
+          </div>
+        )}
+      </>
     </>
   );
 }
